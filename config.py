3968f5d198e99085a59aebe84658ef23d8a95524
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-10-25 12:29:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -70,7 +70,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1014472611)
+ADMINS.append(6502373566)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
