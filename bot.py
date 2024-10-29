09f2dbb186bea272fb1a413e5838a0e3922abfb5
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-10-29 09:50:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,11 +77,11 @@ class Bot(Client):
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
-        except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/nextgenbotz for support")
-            sys.exit()
+           except Exception as e:
+               self.LOGGER(__name__).warning(e)
+               self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+               self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/nextgenbotz for support")
+               sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/nextgenbotz")
