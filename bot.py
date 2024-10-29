cebf6e949c57dc4eb2bb35237bca8398d61a4721
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-10-29 09:46:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,6 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/nextgenbotz for support")
                 sys.exit()
-        try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
